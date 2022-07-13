53993e9eef90c45c035d7045878b54addedf9c86
emilaa <emilaa@code.edu.az>
2022-07-14 03:01:01 +0400

    Add new file

---
+++ Object overriding/Object overriding/Models/MyLIst.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_overriding
+{
+    public class MyLIst <T>
+    {
+        private string[] arr;
+
+        public MyLIst()
+        {
+            arr = new string[0];
+        }
+
+        public void Add(string name)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = name;
+        }
+
+        public void Clear(T data)
+        {
+
+        }
+
+        public void Find(T data)
+        {
+            var result = data;
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Object overriding/Object overriding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object_overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLIst<string> studentList = new MyLIst<string>();
+
+            studentList.Add("Seadet");
+
+            studentList.Clear("Emil");
+
+            studentList.Find("Seid");
+
+            studentList.GetAll();
+        }
+    }
+}
